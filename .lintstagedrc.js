9c97c4ef43622ab26d68ef8226ea4a0397b75bae
david1542 <davidlasry696@gmail.com>
2024-04-12 22:53:13 +0300

    Initial commit

---
+++ .lintstagedrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  '{services,packages,tools}/**/*.{ts,tsx}': files => {
+    return `nx affected --target=typecheck --files=${files.join(',')}`;
+  },
+  '{services,packages,tools}/**/*.{js,ts,jsx,tsx,json}': [
+    files => `nx affected:lint --files=${files.join(',')}`,
+    files => `nx format:write --files=${files.join(',')}`,
+  ],
+  };
