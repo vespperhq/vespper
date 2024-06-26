a32bd33fb24c033c288101bb739aa50d355bf5d3
david1542 <davidlasry696@gmail.com>
2024-06-26 20:21:04 +0300

    Intermediate changes

--- services/api/src/agent/model.ts
+++ services/api/src/agent/model.ts
@@ -2,6 +2,7 @@ import { ChatOpenAI } from "langchain/chat_models/openai";
 import { OpenAIEmbedding } from "llamaindex";
 
 const apiKey = process.env.OPENAI_API_KEY;
+const baseURL = process.env.OPENAI_BASE_URL || "https://api.openai.com/v1/";
 
 export enum ModelName {
   GPT_3_5_TURBO_1106 = "gpt-3.5-turbo-1106",
@@ -15,6 +16,7 @@ export const chatModel = new ChatOpenAI({
   temperature: 0,
   openAIApiKey: apiKey,
   verbose: true,
+  configuration: { baseURL },
 });
 
 export const visionModel = new ChatOpenAI({
@@ -23,10 +25,14 @@ export const visionModel = new ChatOpenAI({
   openAIApiKey: apiKey,
   verbose: true,
   maxTokens: 300,
+  configuration: { baseURL },
 });
 
 export const embedModel = new OpenAIEmbedding({
   apiKey,
   model: "text-embedding-3-large",
   dimensions: 768,
+  azure: {
+    endpoint: baseURL ? baseURL : undefined,
+  },
 });
