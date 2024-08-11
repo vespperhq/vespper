0ce740d0e182c00d934715b22c8a0f3d74ae2fa7
topaztee <topaztee@gmail.com>
2024-08-11 10:45:51 +0300

    fix distinct user id sent to telem

--- services/api/src/app.ts
+++ services/api/src/app.ts
@@ -14,6 +14,7 @@ import {
   featuresRouter,
 } from "./routers";
 import { errorHandler, invalidPathHandler } from "./middlewares/errors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -23,7 +24,7 @@ app.use(
     origin: process.env.DASHBOARD_APP_URL,
   }),
 );
-
+app.use(cookieParser());
 app.use(bodyParser.json({ limit: "1mb" }));
 app.use(express.static("public"));
 
