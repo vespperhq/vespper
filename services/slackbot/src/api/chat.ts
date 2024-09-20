cb965f41c8abe6cef54b79a7a9db5df6ccd3e1e9
david1542 <davidlasry696@gmail.com>
2024-09-20 09:45:11 +0300

    Organize error handling in Slackbot

--- services/slackbot/src/api/chat.ts
+++ services/slackbot/src/api/chat.ts
@@ -29,3 +29,13 @@ export async function getCompletion({
   );
   return response.data;
 }
+
+export const errorMap = {
+  37: `Seems like you haven't created a knowledge graph yet :cry: You must create one so I'll be able to answer your questions.\nPlease go the <https://app.merlinn.co|dashboard> to create one or visit our <https://docs.merlinn.co|docs> to learn more.`,
+  29: `Seems like you are not invited to use me :cry: Make sure an admin invites you, using your Slack email`,
+  30: `Seems like you don't have access to the beta :cry:`,
+  31: `Seems like you haven't accepted your invitation. Please go to your mailbox and accept it.`,
+  32: `Seems like you haven't configured Slack yet 😊 Please go to the dashboard, configure Slack and try again`,
+  35: `Your message seems to violate our terms of use.`,
+  36: `You've exceeded your daily quota of questions. Please try again tommorow or upgrade your plan 😣`,
+};
